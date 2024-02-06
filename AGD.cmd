d335ce2ee29fb4347a38ba687823ba04dcb0f366
Nr2 <info@nicolasrey.com.ar>
2024-02-06 14:34:33 -0300

    go

--- AGD.cmd
+++ AGD.cmd
@@ -4,12 +4,15 @@ rem chcp 65001
 mode con: cols=120 lines=50
 for /f %%a in ('whoami') do set "whoami=%%a"
 
+setlocal enabledelayedexpansion
+
+
 REM Que version soy?
 for %%F in ("%~f0") do set "fileSize=%%~zF"
 cls
 
 Title AGD Toolbox - %whoami% - Version %fileSize%
-echo.
+
 echo AGD Toolbox - v%fileSize%
 echo --------------------
 echo.
@@ -36,8 +39,9 @@ rem ------- help
 IF "%~1"=="help" (
 	echo  * AYUDA *
 	echo.
-	echo    ip : muestra información de red
+	echo    ip : muestra información de red y Windows
 	echo    total : instalar Total Commander
+	echo    reteam : instalar Teamviewer 13
 	echo.
 	echo    noupdate: No intentar actualizarse
 	echo    help: Esta ayuda
@@ -60,10 +64,17 @@ rem ---------------
 rem ------- ip
 IF "%~1"=="ip" (
 
-whoami.exe
-echo.
+for /f %%a in ('wmic computersystem get domain ^| findstr /r /v "^$"') do (set ip_workgroup_domain=%%a)
+
+echo. Host: !ip_workgroup_domain! - %whoami%
 
 ipconfig /all | findstr /v /i /c:"Descrip" /c:"*" /c:"Teredo" | findstr /i /c:"adapt" /c:"Ethernet" /c:"IPv4" /c:"subred" /c:"subnet" /c:"Mask" /c:"Physical" /c:"sica." /c:"Puerta" /c:"Gateway" /c:"192." /c:".0"
+
+echo.
+for /f "delims=" %%i in ('curl.exe ifconfig.me 2^>nul') do set "ip_public=%%i"
+for /f "tokens=2 delims=: " %%a in ('nslookup %ip_public% 2^>nul ^| findstr /C:"Name:" /C:"Nombre:"') do set "ip_hostname=%%a"
+
+echo IP Publica: %ip_public% - %ip_hostname%
 
 echo.
 
@@ -81,11 +92,28 @@ cd "%temp%"
 
 %curl% --remote-name %ftp%/Install/TotalCommanderInstall11.exe
 
-rem "%temp%\TotalCommanderInstall11.exe"
+"%temp%\TotalCommanderInstall11.exe"
 
 )
-
 rem ---------------
+
+
+rem ------- reteam
+IF "%~1"=="reteam" (
+
+echo.
+echo * Instalación de Teamviewer 13
+
+cd "%temp%"
+
+%curl% --remote-name %ftp%/PORTABLES/ReTeam13.exe
+
+"%temp%\ReTeam13.exe"
+
+)
+rem ---------------
+
+
 
 SHIFT
 GOTO parse
