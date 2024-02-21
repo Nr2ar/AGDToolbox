7b1bdf7a78ffe97fd0ed469cf85dd75113da6cee
Nr2 <info@nicolasrey.com.ar>
2024-02-21 10:28:09 -0300

    go

--- AGD.cmd
+++ AGD.cmd
@@ -250,7 +250,7 @@ REM //ANCHOR - Printers
 
 control printers
 
-shell:::{A8A91A66-3A7D-4424-8D24-04E180695C7A}
+explorer shell:::{A8A91A66-3A7D-4424-8D24-04E180695C7A}
 
 goto next
 rem ------------------------------------------------------------------------------------------
