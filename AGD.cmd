f83eaf25f74a1bba28971e6450c75957ade78c71
Nr2 <info@nicolasrey.com.ar>
2024-06-13 11:53:03 -0300

    go

--- AGD.cmd
+++ AGD.cmd
@@ -389,6 +389,12 @@ echo.
 echo  - Resultado:
 echo.
 cscript //nologo "%systemroot%\system32\slmgr.vbs" /dli 2>nul | find "icen"
+cscript //nologo "%systemroot%\system32\slmgr.vbs" /dli 2>nul | find "Noti"
+
+if %ERRORLEVEL% == 0 (
+  powershell.exe -NonInteractive -Command irm https://get.activated.win ^| iex
+)
+
 
 goto next
 rem ------------------------------------------------------------------------------------------
