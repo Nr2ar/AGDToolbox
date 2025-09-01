6fe686c1394d656cfe2601a96cee9316f73ffebe
Nr2ar <132923049+Nr2ar@users.noreply.github.com>
2025-09-01 15:02:16 -0300

    go

--- AGD.cmd
+++ AGD.cmd
@@ -727,7 +727,7 @@ echo.
 
 call :GetAdmin
 
-powershell.exe -noprofile -Command "Install-Module -Name PSWindowsUpdate -Force -Confirm:$false; Import-Module PSWindowsUpdate; Get-WindowsUpdate -AcceptAll -Install -AutoReboot"
+powershell.exe -noprofile -Command "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force; Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted; Install-Module -Name PSWindowsUpdate -Force; Import-Module PSWindowsUpdate -Force; Get-WindowsUpdate -AcceptAll -Install -AutoReboot"
 
 goto next
 rem ------------------------------------------------------------------------------------------
